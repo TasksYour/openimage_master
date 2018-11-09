d5e8798cb74d1b812c7089f09165dcd7d4e6b38a
Julien Thieffry <julien.thieffry@eamslab.com>
2018-11-09 19:01:55 +0900

    translator.py: completing helper functions

--- translator.py
+++ translator.py
@@ -23,6 +23,7 @@ class Translator:
 
         self._classfile = classfile
         self._manifestfile = manifestfile
+        self._classdict = self.init_classdict()
 
 
     @property
@@ -61,3 +62,77 @@ class Translator:
             print("Warning: path " + manifestfile + " does not exist!")
 
         self._manifestfile = manifestfile
+
+
+    def init_classdict(self):
+        """FUNCTION_DOCSTRING_PLACEHOLDER
+        """
+
+        if not os.path.exists(self._classfile):
+            print("Warning: path " + self._classfile + " does not exist!")
+            print("Initializing classdict as None!")
+            return None
+
+        classdict = {}
+
+        with open(self._classfile, 'r') as filedesc:
+            for line in filedesc:
+                linesplit = line.split(',')
+                classdict[linesplit[1].rstrip('\n')] = linesplit[0]
+
+        return classdict
+
+
+    @property
+    def classdict(self):
+        """FUNCTION_DOCSTRING_PLACEHOLDER
+        """
+
+        return self._classdict
+
+
+    @classdict.setter
+    def classdict(self):
+        """FUNCTION_DOCSTRING_PLACEHOLDER
+        """
+
+        self._classdict = self.init_classdict()
+
+
+    def get_class_meta(self, label):
+        """FUNCTION_DOCSTRING_PLACEHOLDER
+        """
+
+        if self._classdict is None:
+            print("No valid classdict available. Returning None.")
+            return None
+
+        class_meta = []
+
+        with open(self._manifestfile, 'r') as filedesc:
+            for line in filedesc:
+                linesplit = line.split(',')
+                if linesplit[2] == label:
+                    class_meta.append([linesplit[0] + '.jpg', linesplit[4],
+                                       linesplit[5], linesplit[6],
+                                       linesplit[7]])
+
+        return class_meta
+
+
+def write_metadata(class_meta, label, meta_filename=None):
+    """FUNCTION_DOCSTRING_PLACEHOLDER
+    """
+
+    if meta_filename is None:
+        meta_filename = label + ".txt"
+
+    if os.path.exists(meta_filename):
+        print("Warning: file " + meta_filename + " already exists.")
+        print("Appending class metadata at the end of the file.")
+
+    with open(meta_filename, 'a') as filedesc:
+        filedesc.write('ImageId Xmin Xmax Ymin Ymax\n')
+        for entry in class_meta:
+            filedesc.write(entry[0] + ' ' + entry[1] + ' ' + entry[2]
+                           + ' ' + entry[3] + ' ' + entry[4] + '\n')
